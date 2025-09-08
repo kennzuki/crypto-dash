d652879ddf24d64c8cd4b875facde7c9226a4655
kennzuki <kingsyken@gmail.com>
2025-09-09 00:49:43 +0300

    limit functionality in url and usestate

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,13 @@ function App() {
   const [coins, setCoins] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const[limit,setLimit] = useState(10);
 
   useEffect(() => {
     async function fetchCoins() {
       try {
         const response = await fetch(
-          `${API_URL}&order=market_cap_desc&per_page=100&page=1&sparkline=false`
+          `${API_URL}&order=market_cap_desc&per_page=${limit}&page=1&sparkline=false`
         );
         if (!response.ok) {
           throw new Error('Network response was not ok');
@@ -27,13 +28,23 @@ function App() {
       }
     }
     fetchCoins();
-  }, []);
+  }, [limit]);
 
   return (
     <div className='bg-black max-w-full flex flex-col justify-center items-center p-16'>
       <section className='flex gap-4 justify-between place-items-center items-center w-full p-4'>
         <h1 className='text-4xl font-bold mb-8 text-white'>Crypto Dashboard</h1>
         <p className='text-white p-4'>Powered by CoinGecko API</p>
+        <div className="border border-gray-300 rounded-md p-2 text-black">
+          <label htmlFor="limit" className="text-white">Show: </label>
+          <select id="limit" className="text-black bg-gray-200 p-2 ml-2" value={limit} onChange={(e) => setLimit(Number(e.target.value))}>
+            <option value="5">5</option>
+            <option value="10">10</option>
+            <option value="20">20</option>
+            <option value="30">30</option>
+            <option value="40">40</option>
+          </select>
+        </div>
       </section>
 
       {loading ? (
