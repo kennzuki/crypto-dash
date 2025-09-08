c568807bb76ca3c05cdb2e5ee0fe2f8156cdea79
kennzuki <kingsyken@gmail.com>
2025-09-08 18:03:45 +0300

    fetching data from coin gecko

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
+import { useState, useEffect, use } from 'react';
 
+const API_URL = import.meta.env.VITE_API_URL;
 
 function App() {
-  
+  const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchCoins() {
+      try {
+        const response = await fetch(
+          `${API_URL}&order=market_cap_desc&per_page=100&page=1&sparkline=false`
+        );
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        setCoins(data);
+        setLoading(false);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchCoins();
+  }, []);
 
   return (
-    <>
-     <h1 className="text-amber-300">ken</h1>
-    </>
-  )
+    <div className="bg-black max-w-full flex flex-col justify-center items-center p-16">
+    
+    </div>
+  );
 }
 
-export default App
+export default App;
