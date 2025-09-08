2f6727bdc4545a6b11cda5bf3ce5f1a26db954e5
kennzuki <kingsyken@gmail.com>
2025-09-09 00:09:02 +0300

    component for coin card

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import CoinCard from "./components/CoinCard";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
@@ -42,22 +43,7 @@ function App() {
       ) : (
         <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
           {coins.map((coin) => (
-            <div key={coin.id} className='bg-gray-800 p-4 rounded-lg shadow-md space-y-2'>
-              
-              <div className="flex gap-2">
-                <img className='w-12 h-12 text-center' src={coin.image} alt={coin.name} />
-                <div className="flex flex-col gap-2">
-                  <h2 className='text-lg font-semibold text-white'>{coin.name}</h2>
-                <p className='text-gray-400'>{coin.symbol.toUpperCase()}</p>
-                </div>
-                
-              </div>
-              
-              <p className='text-gray-400'>Rank: {coin.market_cap_rank}</p>
-              <p className='text-gray-400'>Mrkt Cap: {coin.market_cap.toLocaleString()}</p>
-              <p className='text-gray-400'>Price: {coin.current_price.toLocaleString()}</p>
-              <p className= {coin.price_change_percentage_24h >= 0 ? 'text-green-500' : 'text-red-500'}>Price Change:{coin.price_change_percentage_24h.toFixed(2)}%</p>
-            </div>
+           <CoinCard key={coin.id} coin={coin} />
           ))}
         </div>
       )}
---
+++ src/components/CoinCard.jsx
@@ -0,0 +1,24 @@
+const CoinCard = ({coin}) => {
+    return (
+        <div className="">
+             <div  className='bg-gray-800 p-4 rounded-lg shadow-md space-y-2'>
+              
+              <div className="flex gap-2">
+                <img className='w-12 h-12 text-center' src={coin.image} alt={coin.name} />
+                <div className="flex flex-col gap-2">
+                  <h2 className='text-lg font-semibold text-white'>{coin.name}</h2>
+                <p className='text-gray-400'>{coin.symbol.toUpperCase()}</p>
+                </div>
+                
+              </div>
+              
+              <p className='text-gray-400'>Rank: {coin.market_cap_rank}</p>
+              <p className='text-gray-400'>Mrkt Cap: {coin.market_cap.toLocaleString()}</p>
+              <p className='text-gray-400'>Price: {coin.current_price.toLocaleString()}</p>
+              <p className= {coin.price_change_percentage_24h >= 0 ? 'text-green-500' : 'text-red-500'}>Price Change:{coin.price_change_percentage_24h.toFixed(2)}%</p>
+            </div>
+        </div>
+      );
+}
+ 
+export default CoinCard;
