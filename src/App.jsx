15a25835c4eb9d977fed69fd29d587801fbbd2f3
kennzuki <kingsyken@gmail.com>
2025-09-08 23:53:23 +0300

    mapping card grid for data

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, use } from 'react';
+import { useState, useEffect } from 'react';
 
 const API_URL = import.meta.env.VITE_API_URL;
 
@@ -29,24 +29,34 @@ function App() {
   }, []);
 
   return (
-    <div className="bg-black max-w-full flex flex-col justify-center items-center p-16">
-      <h1 className="text-4xl font-bold mb-8 text-white">Crypto Dashboard</h1>
+    <div className='bg-black max-w-full flex flex-col justify-center items-center p-16'>
+      <section className='flex gap-4 justify-between place-items-center items-center w-full p-4'>
+        <h1 className='text-4xl font-bold mb-8 text-white'>Crypto Dashboard</h1>
+        <p className='text-white p-4'>Powered by CoinGecko API</p>
+      </section>
+
       {loading ? (
         <p>Loading...</p>
       ) : error ? (
         <p>Error: {error}</p>
       ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
           {coins.map((coin) => (
-            <div
-              key={coin.id}
-              className="bg-gray-800 p-4 rounded-lg shadow-md"
-            >
-              <h2 className="text-lg font-semibold text-white">
-                {coin.name} ({coin.symbol})
-              </h2>
-              <p className="text-gray-400">Rank: {coin.market_cap_rank}</p>
-              <p className="text-gray-400">Price: ${coin.current_price}</p>
+            <div key={coin.id} className='bg-gray-800 p-4 rounded-lg shadow-md space-y-2'>
+              
+              <div className="flex gap-2">
+                <img className='w-12 h-12 text-center' src={coin.image} alt={coin.name} />
+                <div className="flex flex-col gap-2">
+                  <h2 className='text-lg font-semibold text-white'>{coin.name}</h2>
+                <p className='text-gray-400'>{coin.symbol.toUpperCase()}</p>
+                </div>
+                
+              </div>
+              
+              <p className='text-gray-400'>Rank: {coin.market_cap_rank}</p>
+              <p className='text-gray-400'>Mrkt Cap: {coin.market_cap.toLocaleString()}</p>
+              <p className='text-gray-400'>Price: {coin.current_price.toLocaleString()}</p>
+              <p className= {coin.price_change_percentage_24h >= 0 ? 'text-green-500' : 'text-red-500'}>Price Change:{coin.price_change_percentage_24h.toFixed(2)}%</p>
             </div>
           ))}
         </div>
