94a2a82fa626f8213a049d5e21426ee636a7e6ba
kennzuki <kingsyken@gmail.com>
2025-09-10 23:15:53 +0300

    not found page

--- src/pages/notFound.jsx
+++ src/pages/notFound.jsx
@@ -2,10 +2,10 @@ import { Link } from "react-router";
 
 const NotFoundPage = () => {
     return ( 
-        <div className="">
+        <div className="mx-auto flex flex-col items-center justify-center h-screen space-y-6">
             <h1 className='text-6xl font-bold'>404</h1>
             <p className='text-2xl font-bold'>oops! the Page was Not Found or doesnt exist</p>
-            <Link to={'/'}>Back to Home page</Link>
+            <Link className="text-blue-500 underline" to={'/'}>Click here to go back to Home page</Link>
         </div>
      );
 }
